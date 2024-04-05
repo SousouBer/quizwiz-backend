60540b98c713b15bf2a72e1d2f4fcdfd8c528b62
Soso <soso.beriashvili@redberry.ge>
2024-04-05 20:09:50 +0400

    refactor: set appropriate status codes and structure auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,6 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Password;
-// use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -22,22 +21,6 @@ use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
-	public function CheckExpration(string $email, string $token)
-	{
-		$passwordResetToken = DB::table('password_reset_tokens')
-		->where('email', $email)
-		->first();
-
-		if (Hash::check($token, $passwordResetToken->token)) {
-			$createdAt = Carbon::parse($passwordResetToken->created_at);
-
-			$expirationTime = $createdAt->addMinutes(config('auth.passwords.users.expire'));
-			if ($expirationTime->isPast()) {
-				return response()->json(['title' => 'Token Expired', 'message' => 'Your token has expired. Request resetting password again.'], 403);
-			}
-		}
-	}
-
 	public function register(RegistrationRequest $request): JsonResponse
 	{
 		$credentials = $request->validated();
@@ -46,20 +29,7 @@ class AuthController extends Controller
 
 		$user->notify(new VerifyEmail($this->verificationUrl($user)));
 
-		return response()->json(['title' => 'Registration Link Sent', 'message' => 'You have successfully created an account. Check your inbox for email verification.'], 200);
-	}
-
-	public function resendEmailVerification(Request $request, string $email): JsonResponse
-	{
-		$user = User::where('email', $email)->first();
-
-		if ($user->hasVerifiedEmail()) {
-			return response()->json(['title' => 'Verified Email', 'message' => 'Your email is already verified. You can log in.'], 422);
-		}
-
-		$user->notify(new VerifyEmail($this->verificationUrl($user)));
-
-		return response()->json(['title' => 'Verification Link Sent', 'message' => 'You have successfully resent an email verification link. Check your inbox!'], 200);
+		return response()->json(['title' => 'Registration Link Sent', 'message' => 'You have successfully created an account. Check your inbox for email verification.'], 201);
 	}
 
 	public function login(LoginRequest $request): JsonResponse
@@ -74,10 +44,10 @@ class AuthController extends Controller
 
 				return response()->json(['title' => 'Login Success', 'message' => 'Your have successfully logged in.'], 200);
 			}
-			return response()->json(['password' => 'Email or password is incorrect'], 404);
+			return response()->json(['password' => 'Email or password is incorrect'], 401);
 		}
 
-		return response()->json(['unverified_user' => 'User has not verified email.'], 404);
+		return response()->json(['unverified_user' => 'User has not verified email.'], 403);
 	}
 
 	public function logout(Request $request): JsonResponse
@@ -88,24 +58,7 @@ class AuthController extends Controller
 
 		$request->session()->regenerateToken();
 
-		return response()->json(['title' => 'Logout Success', 'message' => 'Your have successfully logged out.']);
-	}
-
-	public function resetPassword(PasswordResetRequest $request, string $email, string $token): JsonResponse
-	{
-		$password = $request->validated();
-
-		$user = User::where('email', $email)->firstOrFail();
-
-		$user->forceFill([
-			'password' => Hash::make($password['password']),
-		])->setRememberToken(Str::random(60));
-
-		$user->save();
-
-		event(new PasswordReset($user));
-
-		return response()->json(['title' => 'Password Reset', 'message' => 'Your password has been successfully reset.', 200]);
+		return response()->json(['title' => 'Logout Success', 'message' => 'Your have successfully logged out.'], 200);
 	}
 
 	public function forgotPassword(ForgotPasswordRequest $request): JsonResponse
@@ -125,9 +78,55 @@ class AuthController extends Controller
 		return response()->json(['title' => 'Link Sent', 'message' => 'Your email verification link has been sent. Check your inbox!'], 200);
 	}
 
+	public function resetPassword(PasswordResetRequest $request, string $email, string $token): JsonResponse
+	{
+		$password = $request->validated();
+
+		$user = User::where('email', $email)->firstOrFail();
+
+		$user->forceFill([
+			'password' => Hash::make($password['password']),
+		])->setRememberToken(Str::random(60));
+
+		$user->save();
+
+		event(new PasswordReset($user));
+
+		return response()->json(['title' => 'Password Reset', 'message' => 'Your password has been successfully reset.', 200]);
+	}
+
+	public function resendEmailVerification(Request $request, string $email): JsonResponse
+	{
+		$user = User::where('email', $email)->first();
+
+		if ($user->hasVerifiedEmail()) {
+			return response()->json(['title' => 'Verified Email', 'message' => 'Your email is already verified. You can log in.'], 200);
+		}
+
+		$user->notify(new VerifyEmail($this->verificationUrl($user)));
+
+		return response()->json(['title' => 'Verification Link Sent', 'message' => 'You have successfully resent an email verification link. Check your inbox!'], 200);
+	}
+
+	public function CheckExpration(string $email, string $token)
+	{
+		$passwordResetToken = DB::table('password_reset_tokens')
+		->where('email', $email)
+		->first();
+
+		if (Hash::check($token, $passwordResetToken->token)) {
+			$createdAt = Carbon::parse($passwordResetToken->created_at);
+
+			$expirationTime = $createdAt->addMinutes(config('auth.passwords.users.expire'));
+			if ($expirationTime->isPast()) {
+				return response()->json(['title' => 'Token Expired', 'message' => 'Your token has expired. Request resetting password again.'], 403);
+			}
+		}
+	}
+
 	protected function verificationUrl(User $user): string
 	{
-		$expiration = Carbon::now()->addMinutes(1);
+		$expiration = Carbon::now()->addMinutes(120);
 
 		return URL::temporarySignedRoute(
 			'verification.verify',
