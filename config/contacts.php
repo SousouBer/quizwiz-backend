e85797431fa036058e3619f28504c1e5ac959081
Soso <soso.beriashvili@redberry.ge>
2024-04-29 11:52:09 +0400

    feat: use real data for contacts and create seeder for it

---
+++ config/contacts.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'contacts' => [
+		'email'    => 'example@gmail.com',
+		'tel'      => '+995 599 599 599',
+		'facebook' => 'https://facebook.com',
+		'linkedin' => 'https://linkedin.com',
+	],
+];
