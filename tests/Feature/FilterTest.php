9bea975f501154c11354241bc66774efc9a0dcc5
Soso <soso.beriashvili@redberry.ge>
2024-04-30 19:45:02 +0400

    test: add tests for filter by categories and levels

--- tests/Feature/FilterTest.php
+++ tests/Feature/FilterTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Category;
+use App\Models\DifficultyLevel;
 use App\Models\Quiz;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -24,6 +25,8 @@ class FilterTest extends TestCase
 
 		$response = $this->get(route('quizzes.index'));
 
+		$response->assertSuccessful();
+
 		foreach ($quizzes as $quiz) {
 			$response->assertJsonFragment([
 				'id' => $quiz->id,
@@ -33,15 +36,33 @@ class FilterTest extends TestCase
 
 	public function test_filter_quizzes_are_successfully_filtered_by_categories(): void
 	{
-		$quizzes = Quiz::paginate(9);
-		$category = Category::all()->random();
+		$categoryIDs = Category::inRandomOrder()->take(rand(1, Category::count()))->pluck('id')->toArray();
 
-		$response = $this->get(route('quizzes.index'), []);
+		$categoryIDsString = implode(',', $categoryIDs);
 
-		foreach ($quizzes as $quiz) {
-			$response->assertJsonFragment([
-				'id' => $quiz->id,
-			]);
+		$response = $this->json('GET', route('quizzes.index'), ['categories' => $categoryIDsString]);
+
+		$response->assertSuccessful();
+
+		foreach ($response->json('data') as $quiz) {
+			$ids = array_column($quiz['categories'], 'id');
+
+			$this->assertTrue(!empty(array_intersect($ids, $categoryIDs)));
+		}
+	}
+
+	public function test_filter_quizzes_are_successfully_filtered_by_level(): void
+	{
+		$levelIDs = DifficultyLevel::inRandomOrder()->take(rand(1, DifficultyLevel::count()))->pluck('id')->toArray();
+
+		$levelIDsString = implode(',', $levelIDs);
+
+		$response = $this->json('GET', route('quizzes.index'), ['levels' => $levelIDsString]);
+
+		$response->assertSuccessful();
+
+		foreach ($response->json('data') as $quiz) {
+			$this->assertTrue(in_array($quiz['difficulty_level']['id'], $levelIDs));
 		}
 	}
 }
