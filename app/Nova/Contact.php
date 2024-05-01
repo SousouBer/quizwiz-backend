e3065b97ad451dd2bbf623969835ac7ced51b078
Soso <soso.beriashvili@redberry.ge>
2024-05-02 02:18:11 +0400

    feat: add nova resource for contacts info

---
+++ app/Nova/Contact.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Contact extends Resource
+{
+	public static $model = \App\Models\Contact::class;
+
+	public static $title = 'id';
+
+	public static $search = [
+		'id',
+	];
+
+	public function fields(NovaRequest $request): array
+	{
+		return [
+			ID::make()->sortable(),
+			Text::make('email')->rules('required', 'string'),
+			Text::make('telephone')->rules('required', 'string'),
+			Text::make('facebook')->rules('required', 'string'),
+			Text::make('linkedin')->rules('required', 'string'),
+		];
+	}
+
+	public function cards(NovaRequest $request): array
+	{
+		return [];
+	}
+
+	public function filters(NovaRequest $request): array
+	{
+		return [];
+	}
+
+	public function lenses(NovaRequest $request): array
+	{
+		return [];
+	}
+
+	public function actions(NovaRequest $request): array
+	{
+		return [];
+	}
+}
